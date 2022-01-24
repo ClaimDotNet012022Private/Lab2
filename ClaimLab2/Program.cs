df334c56e4564926271cc83a3847f42294362cd4
Eric Ray <erichamion@gmail.com>
2022-01-23 20:06:19 -0600

    Create solution

---
+++ ClaimLab2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClaimLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
